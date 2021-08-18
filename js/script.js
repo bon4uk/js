bd749260c43d4c0386f25d071db7616028d8cff5
Denis <example@gmail.com>
2021-08-18 16:42:21 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// // const result = confirm("Privet");
+
+// // console.log(result);
+ 
+// // const answer = prompt("апапар", "");
+// // console.log(answer);
+
+// const answers = [];
+
+// // answers[0] = prompt('Как ваше имя?', '');
+// // answers[1] = prompt('Как ваше Фамилия?', '');
+// // answers[2] = prompt('Сколько лет?', '');
+
+// document.write(answers);
+
+console.log('arr' + "- object");
+
+let incr = 10,
+    decr = 10;
+
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);
